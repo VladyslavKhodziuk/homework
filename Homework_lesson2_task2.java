77da7b034f32a61ac1df9c85d6be8ee71ac9561f
Vladyslav Khodziuk <131201436+VladyslavKhodziuk@users.noreply.github.com>
2023-04-28 00:34:17 +0200

    Java. Lesson2. Task2.
    Homework

---
+++ Homework_lesson2_task2.java
@@ -0,0 +1,17 @@
+package lesson2.homwork.task2;
+
+public class Homework_lesson2_task2 {
+    public static void main(String[] args) {
+        
+        double n = 365;
+        double e = n % 10;
+        System.out.println(e);
+
+        double d = n / 10 % 10;
+        System.out.println(d);
+
+        double s = n / 100;
+        System.out.println(s);
+
+    }
+}
