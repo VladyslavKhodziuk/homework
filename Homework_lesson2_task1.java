3332c9d4d95cc1a8346e9e0ff6985c579ad9d771
Vladyslav Khodziuk <131201436+VladyslavKhodziuk@users.noreply.github.com>
2023-04-27 23:51:36 +0200

    Java lesson2  task1
    Homework

---
+++ Homework_lesson2_task1.java
@@ -0,0 +1,30 @@
+package lesson2.homwork.task1;
+
+public class Homework_lesson2_task1 {
+    public static void main(String[] args) {
+
+        char a ='G';
+        System.out.println(a);
+
+        int b = 89;
+        System.out.println(b);
+
+        byte c = 4;
+        System.out.println(c);
+
+        short d = 56;
+        System.out.println(d);
+
+        double e = 4.7333436;
+        System.out.println(e);
+
+        double f = 4.355453532;
+        System.out.println(f);
+
+        long g = 12121;
+        System.out.println(g);
+
+        System.out.println("Note: will change from float 'e' to double 'e', because with float not funcion. Thanks! ");
+    }
+
+}
