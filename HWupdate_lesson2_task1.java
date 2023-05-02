1084091599021fc1932b9d2b5808372d431e4a99
Vladyslav Khodziuk <131201436+VladyslavKhodziuk@users.noreply.github.com>
2023-05-02 22:37:47 +0200

    Homework Updated

---
+++ HWupdate_lesson2_task1.java
@@ -0,0 +1,25 @@
+package lesson2.homwork.task1;
+
+public class HWupdate_lesson2_task1 {
+    public static void main(String[] args) {
+
+        char a ='G';
+        int b = 89;
+        byte c = 4;
+        short d = 56;
+        float e = 4.7333436F;
+        double f = 4.355453532;
+        long g = 12121;
+        boolean z = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(z);
+    }
+
+}
---
+++ HWupdate_lesson2_task2.java
@@ -0,0 +1,15 @@
+package lesson2.homwork.task2;
+
+public class HWupdate_lesson2_task2 {
+    public static void main(String[] args) {
+
+        int n = 365;
+        int e = n % 10;
+        int d = n / 10 % 10;
+        int s = n / 100;
+
+        System.out.println(e);
+        System.out.println(d);
+        System.out.println(s);
+    }
+}
